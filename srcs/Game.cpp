ccb40ba6f2d3caea1d1c6937e8d625634c4990c1
Marius ROBERT <mariusrobert380@gmail.com>
2024-01-18 17:24:18 +0100

    Improved score and remove instant lock on hard drop

--- srcs/Game.cpp
+++ srcs/Game.cpp
@@ -228,6 +228,7 @@ void Game::clearLine(unsigned int line)
 
 void Game::checkLines()
 {
+	int cleared = 0;
 	for (unsigned int i = 0; i < 20; ++i)
 	{
 		bool full = true;
@@ -240,7 +241,26 @@ void Game::checkLines()
 			}
 		}
 		if (full)
+		{
 			clearLine(i);
+			cleared++;
+		}
+	}
+
+	switch (cleared)
+	{
+		case 1:
+			score += 100;
+			break;
+		case 2:
+			score += 300;
+			break;
+		case 3:
+			score += 500;
+			break;
+		case 4:
+			score += 800;
+			break;
 	}
 }
 
@@ -265,7 +285,6 @@ void Game::drop()
 
 	for (int i = 0; i < 4; ++i)
 		tetromino_pos[i] = tetromino_shadow[i];
-	lockTetromino();
 	score += bonus * 2;
 }
 
